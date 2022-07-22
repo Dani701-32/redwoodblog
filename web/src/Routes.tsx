e5e6aaff4fc019d138f98804573d020ed03568b1
Daniel Henrique <85520325+Dani701-32@users.noreply.github.com>
2022-07-22 17:10:51 -0300

    Chapter 2- Getting Dynamic
    Starting using SQL and Migrations to create post in the database

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,9 +1,17 @@
 import { Router, Route, Set } from '@redwoodjs/router'
 
 import BlogLayout from 'src/layouts/BlogLayout'
+import PostsLayout from 'src/layouts/PostsLayout'
+
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
       <Set wrap={BlogLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
