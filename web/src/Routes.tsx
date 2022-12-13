8b1cc8d147ad4f5d3690a6f3ae4824ee9386a24a
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2022-12-13 14:39:31 -0300

    A Second Page and a Link

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
