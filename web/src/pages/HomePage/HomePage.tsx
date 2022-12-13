f2d10f390b58a4330b70c3d68936b4040cb6e053
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2022-12-13 15:47:13 -0300

    Chapter 2 Cells

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
 import { MetaTags } from '@redwoodjs/web'
+import ArticlesCell from 'src/components/ArticlesCell'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      Home
+      <ArticlesCell />
     </>
   )
 }
