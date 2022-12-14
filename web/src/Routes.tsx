6f6ddaa76182d6a9da79def41853e8db6393422a
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2022-12-14 08:26:51 -0300

    Chapter 4 Authentication

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,29 +7,35 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Private, Router, Route, Set } from '@redwoodjs/router'
 
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 import BlogLayout from './layouts/BlogLayout/BlogLayout'
 
 const Routes = () => {
-  return (
-    <Router>
-      <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
-        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
-        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
-        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
-        <Route path="/posts" page={PostPostsPage} name="posts" />
-      </Set>
-      <Set wrap={BlogLayout}>
-        <Route path="/contact" page={ContactPage} name="contact" />
-        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
-        <Route path="/about" page={AboutPage} name="about" />
-        <Route path="/" page={HomePage} name="home" />
-      </Set>
-      <Route notfound page={NotFoundPage} />
-    </Router>
-  )
+    return (
+        <Router>
+            <Route path="/login" page={LoginPage} name="login" />
+            <Route path="/signup" page={SignupPage} name="signup" />
+            <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+            <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+            <Private unauthenticated="home">
+                <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
+                    <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
+                    <Route path="/admin/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+                    <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
+                    <Route path="/admin/posts" page={PostPostsPage} name="posts" />
+                </Set>
+            </Private>
+            <Set wrap={BlogLayout}>
+                <Route path="/contact" page={ContactPage} name="contact" />
+                <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+                <Route path="/about" page={AboutPage} name="about" />
+                <Route path="/" page={HomePage} name="home" />
+            </Set>
+            <Route notfound page={NotFoundPage} />
+        </Router>
+    )
 }
 
 export default Routes
--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -1,33 +1,48 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 
 type BlogLayoutProps = {
-  children?: React.ReactNode
+    children?: React.ReactNode
 }
 
 const BlogLayout = ({ children }: BlogLayoutProps) => {
-  return (
-    <>
-      <header>
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.home()}>Home</Link>
-            </li>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-            <li>
-              <Link to={routes.contact()}>Contact Us</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>{children}</main>
-    </>
-  )
+    const { isAuthenticated, currentUser, logOut } = useAuth()
+    return (
+        <>
+            <header>
+                <div className="flex-between">
+                    <h1>
+                        <Link to={routes.home()}>Redwood Blog</Link>
+                    </h1>
+                    {isAuthenticated ? (
+                        <div>
+                            <span>Logged in as {currentUser.email}</span>
+                            {''}
+                            <button type="button" onClick={logOut}>
+                                Logout
+                            </button>
+                        </div>
+                    ) : (
+                        <Link to={routes.login()}>Login</Link>
+                    )}
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to={routes.home()}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={routes.about()}>About</Link>
+                        </li>
+                        <li>
+                            <Link to={routes.contact()}>Contact Us</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main>{children}</main>
+        </>
+    )
 }
 
 export default BlogLayout
