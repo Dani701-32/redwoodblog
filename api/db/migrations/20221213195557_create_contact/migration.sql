a0e0f46c54288e8e2c7f2e7522f9689ac357bd02
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2022-12-14 10:06:09 -0300

    Chapter Deployment Netlify

--- api/db/migrations/20221213195557_create_contact/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Contact" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT,
-    "email" TEXT NOT NULL,
-    "message" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
