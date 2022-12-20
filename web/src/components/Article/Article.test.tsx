4e5c27e02d0e387ce59f3eb8f3470d31447ae534
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2022-12-20 11:48:29 -0300

    Chapter 5 Our First Test

--- web/src/components/Article/Article.test.tsx
+++ web/src/components/Article/Article.test.tsx
@@ -1,14 +1,31 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen } from '@redwoodjs/testing/web'
 
 import Article from './Article'
 
 //   Improve this test with help from the Redwood Testing Doc:
 //    https://redwoodjs.com/docs/testing#testing-components
-
+const ARTICLE = {
+    id: 1,
+    title: 'First post',
+    body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
+    createdAt: new Date().toISOString(),
+}
 describe('Article', () => {
-  it('renders successfully', () => {
-    expect(() => {
-      render(<Article />)
-    }).not.toThrow()
-  })
+    it('renders a blog post', () => {
+        render(<Article article={ARTICLE} />)
+
+        expect(screen.getByText(ARTICLE.title)).toBeInTheDocument()
+        expect(screen.getByText(ARTICLE.body)).toBeInTheDocument()
+    })
+
+    it('renders a summary of a blog post', () => {
+        render(<Article article={ARTICLE} summary={true} />)
+
+        expect(screen.getByText(ARTICLE.title)).toBeInTheDocument()
+        expect(
+            screen.getByText(
+                'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Str...'
+            )
+        ).toBeInTheDocument()
+    })
 })
--- web/src/components/ArticlesCell/ArticlesCell.test.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen, within } from '@redwoodjs/testing/web'
+
 import { Loading, Empty, Failure, Success } from './ArticlesCell'
 import { standard } from './ArticlesCell.mock'
 
@@ -9,33 +10,45 @@ import { standard } from './ArticlesCell.mock'
 // https://redwoodjs.com/docs/testing#jest-expect-type-considerations
 
 describe('ArticlesCell', () => {
-  it('renders Loading successfully', () => {
-    expect(() => {
-      render(<Loading />)
-    }).not.toThrow()
-  })
+    it('renders Loading successfully', () => {
+        expect(() => {
+            render(<Loading />)
+        }).not.toThrow()
+    })
 
-  it('renders Empty successfully', async () => {
-    expect(() => {
-      render(<Empty />)
-    }).not.toThrow()
-  })
+    it('renders Empty successfully', async () => {
+        expect(() => {
+            render(<Empty />)
+        }).not.toThrow()
+    })
 
-  it('renders Failure successfully', async () => {
-    expect(() => {
-      render(<Failure error={new Error('Oh no')} />)
-    }).not.toThrow()
-  })
+    it('renders Failure successfully', async () => {
+        expect(() => {
+            render(<Failure error={new Error('Oh no')} />)
+        }).not.toThrow()
+    })
 
-  // When you're ready to test the actual output of your component render
-  // you could test that, for example, certain text is present:
-  //
-  // 1. import { screen } from '@redwoodjs/testing/web'
-  // 2. Add test: expect(screen.getByText('Hello, world')).toBeInTheDocument()
+    // When you're ready to test the actual output of your component render
+    // you could test that, for example, certain text is present:
+    //
+    // 1. import { screen } from '@redwoodjs/testing/web'
+    // 2. Add test: expect(screen.getByText('Hello, world')).toBeInTheDocument()
 
-  it('renders Success successfully', async () => {
-    expect(() => {
-      render(<Success articles={standard().articles} />)
-    }).not.toThrow()
-  })
+    it('renders Success successfully', async () => {
+        const articles = standard().articles
+        render(<Success articles={articles} />)
+        articles.forEach((article) => {
+            const truncatedBody = article.body.substring(0, 10)
+            const matchedBody = screen.getByText(truncatedBody, {
+                exact: false,
+            })
+            const ellipsis = within(matchedBody).getByText('...', {
+                exact: false,
+            })
+
+            expect(screen.getByText(article.title)).toBeInTheDocument()
+            expect(screen.queryByText(article.body)).not.toBeInTheDocument()
+            expect(ellipsis).toBeInTheDocument()
+        })
+    })
 })
